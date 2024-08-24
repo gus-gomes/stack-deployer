bd2bc058abc08c6f30e609a7797a0243555ddbde
Gustavo Gomes <gustavo_gomes94@hotmail.com>
2024-08-24 23:08:19 +0100

    Delete creds from terraform scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+## Exposed
+## Fix: https://medium.com/@magelan09/terraform-encrypted-variables-how-not-to-commit-sensitive-information-to-a-git-58d51e462e1a
+provider "proxmox" {
+ pm_api_url   = "https://<ip-addr>:8006/api2/json"
+ pm_api_token_id      = "terraform-prov@pve!infra"
+ pm_api_token_secret  = ""
+ pm_tls_insecure = true
+}
+
 
 resource "proxmox_vm_qemu" "resource-name" {
   name        = "VM-name"
--- terraform/vars.tfvars
+++
@@ -1,8 +0,0 @@
-## Exposed
-## Fix: https://medium.com/@magelan09/terraform-encrypted-variables-how-not-to-commit-sensitive-information-to-a-git-58d51e462e1a
-provider "proxmox" {
- pm_api_url   = "https://192.168.1.213:8006/api2/json"
- pm_api_token_id      = "terraform-prov@pve!infra"
- pm_api_token_secret  = "1a2c9c64-1be1-4c29-aecc-3a3f6270518e"
- pm_tls_insecure = true
-}
