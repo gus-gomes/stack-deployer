dd9b3fa84f6b741275af98a8c9030f2f7b3b89d7
Gustavo Gomes <gustavogomes@Gustavos-MacBook-Pro.local>
2024-12-18 22:04:28 +0000

    remove ssh key

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -52,3 +52,9 @@ variable "proxmox_host" {
   description = "The Proxmox host IP or hostname"
   type        = string
 }
+
+variable "ssh_public_key" {
+  description = "The public SSH key for accessing resources"
+  type        = string
+  sensitive   = true
+}
